d7d8944384c91e68933910b92fc06196cc96a044
yk <1245760621@qq.com>
2021-07-09 20:54:02 +0700

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var sshClient = new SshClient("127.0.0.1", 2222, "root", "vagrant"))
+            {
+                sshClient.Connect();
+
+                string cmd1 = "cd /;ls -l";
+
+                using (var cmd = sshClient.CreateCommand(cmd1))
+                {
+                    var res = cmd.Execute();
+
+                    var arr = res.Split('\n');
+                    var arr2 = arr[1].Split('\t');
+                    Console.Write(res);
+
+                    Console.ReadKey();
+
+
+                }
+            }
+        }
+    }
+}
